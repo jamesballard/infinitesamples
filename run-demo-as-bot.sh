26eb58e49aa105efa72f5bb77a143d9295154857
Robert Egglestone <robert@infiniterooms.co.uk>
2013-05-24 19:21:58 +0000

    Prevent duplicate runs

--- run-demo-as-bot.sh
+++ run-demo-as-bot.sh
@@ -4,4 +4,5 @@ out=/var/log/infinitebot/demo.out
 log=/var/log/infinitebot/demo.log
 results=/var/log/infinitebot/demo.result
 host=demo.infiniterooms.co.uk
+pkill -9 -U infinitebot java
 exec su -s /bin/bash infinitebot -c "JAVA_CMD=java JVM_ARGS='-Xmx128m' jmeter -n -X -j $log -l $results -t $plan -Jhost=$host -Jprotocol=https -Jport=443 -Jpath=/moodle &> $out"
